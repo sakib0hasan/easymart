a1765c9663751f2ce71162d15c94bcd237e36565
Gulshan Jubaed Prince <jubaedprince@hotmail.com>
2015-01-09 06:09:15 -0500

    Key changed by prince

--- app/config/app.php
+++ app/config/app.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'debug' => false,
+	'debug' => true,
 
 	/*
 	|--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return array(
 	|
 	*/
 
-	'key' => '',
+	'key' => 'asdfasfwewer1324',
 
 	'cipher' => MCRYPT_RIJNDAEL_128,
 
